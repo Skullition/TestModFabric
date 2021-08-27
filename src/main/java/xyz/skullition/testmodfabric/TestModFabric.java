6ffc030139b9f5d057e44ae8af8462c7137f5731
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-27 19:50:22 +0700

    Finish initial commit

--- src/main/java/xyz/skullition/testmodfabric/TestModFabric.java
+++ src/main/java/xyz/skullition/testmodfabric/TestModFabric.java
@@ -1,8 +1,14 @@
-package net.fabricmc.example;
+package xyz.skullition.testmodfabric;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class TestModFabric implements ModInitializer {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String NAMESPACE = "testmodfabric";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("Hello Fabric world!");
+
 	}
 }
