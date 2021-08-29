f595e10a59474556b0f6fe24dfef7a03842cb09c
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-29 16:15:05 +0700

    Add property-less unsafe block

---
+++ src/main/java/xyz/skullition/testmodfabric/blocks/UnsafeBlock.java
@@ -0,0 +1,9 @@
+package xyz.skullition.testmodfabric.blocks;
+
+import net.minecraft.block.Block;
+
+public class UnsafeBlock extends Block {
+    public UnsafeBlock(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/xyz/skullition/testmodfabric/registry/Setup.java
+++ src/main/java/xyz/skullition/testmodfabric/registry/Setup.java
@@ -2,16 +2,23 @@ package xyz.skullition.testmodfabric.registry;
 
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.skullition.testmodfabric.TestModFabric;
+import xyz.skullition.testmodfabric.blocks.UnsafeBlock;
 import xyz.skullition.testmodfabric.items.RealPhone;
 
 public class Setup {
 
     public static final RealPhone REAL_PHONE = new RealPhone(new FabricItemSettings().maxCount(16).group(TestModFabric.MY_ITEM_GROUP).fireproof());
+    public static final UnsafeBlock UNSAFE_BLOCK = new UnsafeBlock(FabricBlockSettings.of(Material.GLASS).breakByHand(true).drops(new Identifier(TestModFabric.MODID, "unsafeblock")));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "realphone"), REAL_PHONE);
+        Registry.register(Registry.BLOCK, new Identifier(TestModFabric.MODID, "unsafeblock"), UNSAFE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "unsafeblock"), new BlockItem(UNSAFE_BLOCK, new FabricItemSettings().group(TestModFabric.MY_ITEM_GROUP)));
     }
 }
