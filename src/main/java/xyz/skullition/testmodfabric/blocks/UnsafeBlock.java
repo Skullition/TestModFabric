24198de37f3e6e7aee607874c6331fe03e1b4230
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-29 20:03:13 +0700

    add toggleable BooleanProperty to unsafeblock

--- src/main/java/xyz/skullition/testmodfabric/blocks/UnsafeBlock.java
+++ src/main/java/xyz/skullition/testmodfabric/blocks/UnsafeBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -19,28 +20,33 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class UnsafeBlock extends Block {
-    public static final BooleanProperty CHARGED = BooleanProperty.of("charged");
+    public static final BooleanProperty LIGHTNING_THING = BooleanProperty.of("lightningthing");
 
     public UnsafeBlock(Settings settings) {
         super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(CHARGED, false));
+        setDefaultState(getStateManager().getDefaultState().with(LIGHTNING_THING, false));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(CHARGED);
+        builder.add(LIGHTNING_THING);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        world.playSound(player, pos, SoundEvents.AMBIENT_UNDERWATER_ENTER, SoundCategory.HOSTILE, 1F, 1F);
-        world.setBlockState(pos, state.with(CHARGED, true));
+        // check if block is active
+        if (world.getBlockState(pos).get(LIGHTNING_THING).equals(true) && !world.isClient()) {
+            world.setBlockState(pos,state.with(LIGHTNING_THING, false));
+        } else {
+            world.playSound(player, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_HIT, SoundCategory.HOSTILE, 1F, 1F);
+            world.setBlockState(pos, state.with(LIGHTNING_THING, true));
+        }
         return ActionResult.SUCCESS;
     }
 
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if (entity.isPlayer() && world.getBlockState(pos).get(CHARGED)) {
+        if (entity.isPlayer() && world.getBlockState(pos).get(LIGHTNING_THING)) {
             PigEntity pigEntity = EntityType.PIG.create(world);
             LightningEntity lightningEntity = EntityType.LIGHTNING_BOLT.create(world);
             if (lightningEntity != null) {
@@ -51,7 +57,7 @@ public class UnsafeBlock extends Block {
             }
             world.spawnEntity(pigEntity);
             world.spawnEntity(lightningEntity);
-            world.setBlockState(pos, state.with(CHARGED, false));
+            world.setBlockState(pos, state.with(LIGHTNING_THING, false));
         }
         super.onSteppedOn(world, pos, state, entity);
     }
