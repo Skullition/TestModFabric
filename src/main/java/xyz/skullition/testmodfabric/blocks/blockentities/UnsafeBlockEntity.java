5d14e31f7373bfe5666869b2e1c6f3b12c3f49f0
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-02 09:14:24 +0700

    Override writeNbt and readNbt in UnsafeBlockEntity

--- src/main/java/xyz/skullition/testmodfabric/blocks/blockentities/UnsafeBlockEntity.java
+++ src/main/java/xyz/skullition/testmodfabric/blocks/blockentities/UnsafeBlockEntity.java
@@ -7,15 +7,22 @@ import net.minecraft.util.math.BlockPos;
 import xyz.skullition.testmodfabric.registry.Setup;
 
 public class UnsafeBlockEntity extends BlockEntity {
+    private int testInt = 4;
+
     public UnsafeBlockEntity(BlockPos pos, BlockState state) {
         super(Setup.UNSAFE_BLOCK_ENTITY, pos, state);
     }
 
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
-        int testInt = 4;
         super.writeNbt(nbt);
         nbt.putInt("somenumber", testInt);
         return nbt;
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        testInt = nbt.getInt("somenumber");
     }
 }
