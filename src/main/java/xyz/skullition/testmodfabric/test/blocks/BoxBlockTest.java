d2d1797a95a0d6baef086608bf9a36aeb9ee837c
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-11 21:47:15 +0700

    Add a test

--- src/main/java/xyz/skullition/testmodfabric/test/blocks/BoxBlockTest.java
+++ src/main/java/xyz/skullition/testmodfabric/test/blocks/BoxBlockTest.java
@@ -1,6 +1,9 @@
 package xyz.skullition.testmodfabric.test.blocks;
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
@@ -18,6 +21,21 @@ public class BoxBlockTest implements FabricGameTest {
         boxBlockEntity.setStack(0, Items.SNOWBALL.getDefaultStack());
         test.addInstantFinalTask(() -> {
             test.expectContainerWith(pos, Items.SNOWBALL);
+        });
+    }
+
+    @GameTest(structureName = EMPTY_STRUCTURE)
+    public void checkInventoryWhenBroken(TestContext test) {
+        BlockPos pos = new BlockPos(2, 2, 2);
+        test.setBlockState(pos, Setup.BOX_BLOCK);
+        test.setBlockState(2, 0, 2, Blocks.DIRT);
+        BoxBlockEntity boxBlockEntity = (BoxBlockEntity) test.getBlockEntity(pos);
+        boxBlockEntity.setStack(0, Items.BONE_BLOCK.getDefaultStack());
+         ItemStack stack = boxBlockEntity.getStack(0);
+         stack.setCount(64);
+        test.removeBlock(pos);
+        test.addInstantFinalTask(() -> {
+            test.expectItemsAt(Items.BONE_BLOCK, new BlockPos(2, 1, 2), 1, 64);
         });
     }
 }
