2c95893a7e1e556aa2da9243b7a5d68c4181da4a
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-11 19:46:04 +0700

    Add some tests

--- src/main/java/xyz/skullition/testmodfabric/test/blocks/UnsafeBlockTest.java
+++ src/main/java/xyz/skullition/testmodfabric/test/blocks/UnsafeBlockTest.java
@@ -1,6 +1,7 @@
 package xyz.skullition.testmodfabric.test.blocks;
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
@@ -21,4 +22,17 @@ public class UnsafeBlockTest implements FabricGameTest {
         test.method_36038(EntityType.ZOMBIFIED_PIGLIN, new BlockPos(2, 2, 2));
         test.killAllEntities();
     }
+
+    @GameTest(structureName = EMPTY_STRUCTURE)
+    public void checkExplosion(TestContext test) {
+        BlockPos blockPos = new BlockPos(2, 1, 2);
+
+        test.setBlockState(blockPos, Setup.UNSAFE_BLOCK);
+        test.setBlockState(3, 1, 2, Setup.UNSAFE_BLOCK);
+        test.expectBlock(Setup.UNSAFE_BLOCK, blockPos);
+        test.useBlock(blockPos);
+        test.spawnEntity(EntityType.ARROW, 2, 5, 2);
+        test.expectBlockAtEnd(Blocks.AIR, 3, 1, 2);
+    }
+
 }
