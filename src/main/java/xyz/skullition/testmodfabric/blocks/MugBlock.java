9ecf2094f17b07bb5b79e890d23af8b72d15c95e
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-26 14:11:21 +0700

    Create MugBlock

---
+++ src/main/java/xyz/skullition/testmodfabric/blocks/MugBlock.java
@@ -0,0 +1,20 @@
+package xyz.skullition.testmodfabric.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class MugBlock extends Block {
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 6.0D, 11.0D);
+    public MugBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+}
--- src/main/java/xyz/skullition/testmodfabric/registry/Setup.java
+++ src/main/java/xyz/skullition/testmodfabric/registry/Setup.java
@@ -18,6 +18,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.skullition.testmodfabric.TestModFabric;
 import xyz.skullition.testmodfabric.blocks.BoxBlock;
+import xyz.skullition.testmodfabric.blocks.MugBlock;
 import xyz.skullition.testmodfabric.blocks.PurpleGlassBlock;
 import xyz.skullition.testmodfabric.blocks.UnsafeBlock;
 import xyz.skullition.testmodfabric.blocks.blockentities.BoxBlockEntity;
@@ -36,6 +37,7 @@ public class Setup {
     public static final LightningStaff LIGHTNING_STAFF = new LightningStaff(new FabricItemSettings().maxCount(1).group(TestModFabric.MY_ITEM_GROUP));
     public static final UnsafeBlock UNSAFE_BLOCK = new UnsafeBlock(FabricBlockSettings.of(Material.GLASS).breakByHand(true).drops(new Identifier(TestModFabric.MODID, "unsafeblock")));
     public static final BoxBlock BOX_BLOCK = new BoxBlock(FabricBlockSettings.of(Material.WOOD).breakByHand(true).drops(new Identifier(TestModFabric.MODID, "boxblock")));
+    public static final MugBlock MUG_BLOCK = new MugBlock(FabricBlockSettings.of(Material.GLASS).nonOpaque());
     public static final PurpleGlassBlock PURPLE_GLASS_BLOCK = new PurpleGlassBlock(FabricBlockSettings.of(Material.GLASS).nonOpaque().breakByHand(true).drops(new Identifier(TestModFabric.MODID, "purpleglassblock")));
     public static final ScreenHandlerType<BoxScreenHandler> BOX_SCREEN_HANDLER = ScreenHandlerRegistry.registerExtended(new Identifier(TestModFabric.MODID, "boxblock"), BoxScreenHandler::new);
     public static final EntityType<ThingEntity> THING_ENTITY = FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, ThingEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build();
@@ -52,10 +54,12 @@ public class Setup {
         Registry.register(Registry.BLOCK, new Identifier(TestModFabric.MODID, "unsafeblock"), UNSAFE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(TestModFabric.MODID, "purpleglassblock"), PURPLE_GLASS_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(TestModFabric.MODID, "boxblock"), BOX_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(TestModFabric.MODID, "mugblock"), MUG_BLOCK);
         // block items
         Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "unsafeblock"), new BlockItem(UNSAFE_BLOCK, new FabricItemSettings().group(TestModFabric.MY_ITEM_GROUP)));
         Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "purpleglassblock"), new BlockItem(PURPLE_GLASS_BLOCK, new FabricItemSettings().group(TestModFabric.MY_ITEM_GROUP)));
         Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "boxblock"), new BlockItem(BOX_BLOCK, new FabricItemSettings().group(TestModFabric.MY_ITEM_GROUP)));
+        Registry.register(Registry.ITEM, new Identifier(TestModFabric.MODID, "mugblock"), new BlockItem(MUG_BLOCK, new FabricItemSettings().group(TestModFabric.MY_ITEM_GROUP)));
         // block entities
         Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(TestModFabric.MODID, "unsafeblockentity"), UNSAFE_BLOCK_ENTITY);
         Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(TestModFabric.MODID, "boxblockentity"), BOX_BLOCK_ENTITY);
