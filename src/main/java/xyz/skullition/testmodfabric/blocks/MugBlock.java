d328c7c91a34bb2d2ef9c9c4259cf9d981dfd4c2
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-26 16:42:20 +0700

    Fix MugBlock outline

--- src/main/java/xyz/skullition/testmodfabric/blocks/MugBlock.java
+++ src/main/java/xyz/skullition/testmodfabric/blocks/MugBlock.java
@@ -8,7 +8,7 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
 public class MugBlock extends Block {
-    protected static final VoxelShape SHAPE = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 6.0D, 11.0D);
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 5.0D, 11.0D);
     public MugBlock(Settings settings) {
         super(settings);
     }
